e6bf38092c7de0d95d8414ab4e32db5de0a95909
Najima Sahloumi <nsahloum@w-r7-p5.s19.be>
2020-01-27 15:10:10 +0100

    lecture fichier ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,32 @@
 /*   By: nsahloum <nsahloum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 19:57:40 by nsahloum          #+#    #+#             */
-/*   Updated: 2020/01/26 21:47:38 by nsahloum         ###   ########.fr       */
+/*   Updated: 2020/01/27 15:06:50 by nsahloum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int get_next_line(int fd, char **line)
+#include "get_next_line.h"
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int get_next_line(int fd)
 {
+	char *str;
 	
+	if (!(str = malloc(sizeof(char) * BUFFER_SIZE + 1)))
+		return (0);
+	read(fd, str, BUFFER_SIZE);
+	printf("%s", str);
+	return (1);
+}
+
+int main(void)
+{
+	int fd;
+	fd = open("numbers.dict", O_RDONLY);
+	get_next_line(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,17 @@
 /*   By: nsahloum <nsahloum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 20:04:33 by nsahloum          #+#    #+#             */
-/*   Updated: 2020/01/26 20:04:34 by nsahloum         ###   ########.fr       */
+/*   Updated: 2020/01/27 15:04:04 by nsahloum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 12
+# endif
+
+#endif
